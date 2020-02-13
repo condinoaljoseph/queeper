ebec25b07c4f74d61414acb720e1dd6c8841e025
Al Joseph Condino <condinoaljoseph@gmail.com>
2020-02-13 11:56:38 +0800

    express server setup with initial routes

---
+++ routes/contacts.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+
+// @route     GET api/contacts
+// @desc      Get all users contacts
+// @access    Private
+
+router.get('/', (req, res) => {
+  res.send('Get all users contacts');
+});
+
+// @route     POST api/contacts
+// @desc      Add new contacts
+// @access    Private
+
+router.post('/', (req, res) => {
+  res.send('Add new contacts');
+});
+
+// @route     PUT api/contacts:id
+// @desc      Update contacts
+// @access    Private
+
+router.put('/:id', (req, res) => {
+  res.send('Update contact');
+});
+
+// @route     DELETE api/contacts:id
+// @desc      delete contacts
+// @access    Private
+
+router.delete('/:id', (req, res) => {
+  res.send('Delete contact');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to ContactKeeper' }));
+
+// DEFINE routes
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
